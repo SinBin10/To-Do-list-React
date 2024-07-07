52e848cb172a7fedd716b9b6227ddfab83bff291
SinBin10 <bincode10@gmail.com>
2024-07-07 13:06:09 +0530

    done with modal

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -1,5 +1,20 @@
+import "./Modal.css";
+
 const Modal = () => {
-  return <>modal</>;
+  return (
+    <div className="add-task">
+      <div className="task">
+        <div className="task-heading">
+          <label>Enter Heading..</label>
+          <input className="heading-input" type="text" />
+        </div>
+        <div className="task-body">
+          <label>Enter Task details..</label>
+          <textarea className="body-input"></textarea>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Modal;

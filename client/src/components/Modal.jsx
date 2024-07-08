1a4ffb6860b0cc5a7de3d962f133e2147c7534b9
SinBin10 <bincode10@gmail.com>
2024-07-08 14:29:32 +0530

    done with modal

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -1,19 +1,25 @@
 import "./Modal.css";
 
-const Modal = () => {
+const Modal = ({ onChange }) => {
   return (
-    <div className="add-task">
-      <div className="task">
-        <div className="task-heading">
-          <label>Enter Heading..</label>
-          <input className="heading-input" type="text" />
-        </div>
-        <div className="task-body">
-          <label>Enter Task details..</label>
-          <textarea className="body-input"></textarea>
+    <>
+      <div className="add-task" onClick={onChange}>
+        <div className="task" onClick={(e) => e.stopPropagation()}>
+          <div className="task-heading">
+            <label>Enter Heading..</label>
+            <input className="heading-input" type="text" />
+          </div>
+          <div className="task-body">
+            <label>Enter Task details..</label>
+            <textarea className="body-input"></textarea>
+          </div>
+          <div className="btn">
+            <button>Save</button>
+            <button onClick={onChange}>Cancel</button>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,20 @@
 import { Link } from "react-router-dom";
+import Modal from "../components/Modal";
+import { useState } from "react";
 import "./Home.css";
 
 const Home = () => {
+  const [displaymodal, setDisplaymodal] = useState(false);
+  function cancelClick() {
+    setDisplaymodal(false);
+  }
   return (
     <>
+      {displaymodal && <Modal onChange={cancelClick} />}
       <div className="home-page">
-        <button className="add-new-btn">Add New Task</button>
+        <button onClick={() => setDisplaymodal(true)} className="add-new-btn">
+          Add New Task
+        </button>
         <Link className="saved-lists-btn" to="/saved_lists">
           <button>Saved Lists</button>
         </Link>
